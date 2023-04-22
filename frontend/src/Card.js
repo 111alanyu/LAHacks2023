2f7fac92a820e5af364a87805f0abe5b6c9ecb73
Alan Yu <alanyu111@g.ucla.edu>
2023-04-22 14:59:52 -0700

    Finished Card

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -1,21 +1,68 @@
 import './card.css'
+import thumbtack from './thumbtack.png';
+import face from "./face.jpg"
+
 
 function Card() {
     return (
         <div className="Card-Header">
+
+            <div className='container'>
+                <img src={face} alt="My Image" class="face"></img>
+                <img src={thumbtack} alt="My Image" class="thumb"></img>
+            </div>
+
+
             <header className="old-paper">
-                <div className="old-text">
-                    <h1>Alan's Card</h1>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                    <a
-                        className="App-link"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                    </a>
+
+                <br></br>
+
+                <div className="typed-text">
+                    Name:
                 </div>
+                <div className="old-text">
+                    Alan's Card
+                </div>
+
+                <br></br>
+
+                <div className="typed-text">
+                    Home-Town:
+                </div>
+                <div className="old-text">
+                    San Jose
+                </div>
+
+                <br></br>
+
+                <div className="typed-text">
+                    Time-Met:
+                </div>
+                <div className="old-text">
+                    15:15:23PST
+                </div>
+
+
+
+                <br></br>
+                <br></br>
+
+
+
+                <div className="typed-text">
+                    Remarks:
+                </div>
+                <div className="old-text">
+                    <p>
+                        Hey there! I'm just your average lover of all things coffee, good music, and dogs. You can usually find me at the local coffee shop, sipping on a latte and listening to my favorite indie tunes. </p>
+                </div>
+
+                <a
+                    className="App-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
             </header>
         </div>
     );
