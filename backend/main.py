816d955333021d135cd6379449d4ce50a59ad705
David Davini <dndavini3.14@gmail.com>
2023-04-22 15:36:59 -0700

    fix hot reload

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 # Should redirect to /login if not logged in,
 # and to /stats if logged in
 @app.route("/")
 def api_index():
     return "index"
 
+
 @app.route("/login")
 def api_login():
     return "login"
+
 
 # If not logged in, redirect to login.
 # Redirect back after logging in.
@@ -18,9 +21,12 @@ def api_login():
 def api_scan(username):
     return f"Scanned {username}"
 
+
 # Same as above. Double redirect if not logged in.
 @app.route("/stats/<username>")
 def api_stats(username):
+    pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
