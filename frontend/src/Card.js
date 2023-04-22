21764f512354cb178831e4c7c3975075402ca04a
Alan Yu <alanyu111@g.ucla.edu>
2023-04-22 13:25:04 -0700

    Added new files

---
+++ frontend/src/Card.js
@@ -0,0 +1,24 @@
+import './card.css'
+
+function Card() {
+    return (
+        <div className="Card-Header">
+            <header className="old-paper">
+                <div className="old-text">
+                    <h1>Alan's Card</h1>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    <a
+                        className="App-link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                    </a>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Card;
