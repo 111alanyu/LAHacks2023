744a9304d56046ad3d3ca93058a814940358e3d7
Alan Yu <alanyu111@g.ucla.edu>
2023-04-22 15:57:20 -0700

    Fix part 7

--- frontend/src/utils/Card.js
+++ frontend/src/utils/Card.js
@@ -1,13 +1,71 @@
 import './Card.css'
+import thumbtack from './thumbtack.png';
+import face from "./face.jpg"
 
-const Card = (props) => {
+
+function Card() {
   return (
-    <div className='card'>
-      <p>Name: {props.name}</p>
-      <p>Hometown: {props.hometown}</p>
-      <p>Notes: {props.notes}</p>
+    <div className="Card-Header">
+
+      <div className='container'>
+        <img src={face} alt="My Image" class="face"></img>
+        <img src={thumbtack} alt="My Image" class="thumb"></img>
+      </div>
+
+
+      <header className="old-paper">
+
+        <br></br>
+
+        <div className="typed-text">
+          Name:
+        </div>
+        <div className="old-text">
+          Alan's Card
+        </div>
+
+        <br></br>
+
+        <div className="typed-text">
+          Home-Town:
+        </div>
+        <div className="old-text">
+          San Jose
+        </div>
+
+        <br></br>
+
+        <div className="typed-text">
+          Time-Met:
+        </div>
+        <div className="old-text">
+          15:15:23PST
+        </div>
+
+
+
+        <br></br>
+        <br></br>
+
+
+
+        <div className="typed-text">
+          Remarks:
+        </div>
+        <div className="old-text">
+          <p>
+            Hey there! I'm just your average lover of all things coffee, good music, and dogs. You can usually find me at the local coffee shop, sipping on a latte and listening to my favorite indie tunes. </p>
+        </div>
+
+        <a
+          className="App-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
 export default Card;
