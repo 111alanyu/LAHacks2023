82a29d32f776f94c4f8b8d392a718a45ee61240d
Alan Yu <alanyu111@g.ucla.edu>
2023-04-22 23:24:06 -0700

    Happy place with map

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import Map from './utils/map';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Map />
   </React.StrictMode>
 );
 
