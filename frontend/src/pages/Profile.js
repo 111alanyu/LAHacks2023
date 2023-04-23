123b83a58d68700c3dfd302a21fca2867e5337ff
jenetic <jznnylam@gmail.com>
2023-04-22 23:31:25 -0700

    Connect profile updating to firebase

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,31 +2,38 @@ import { useEffect, useState } from 'react';
 import Card from '../utils/Card';
 import './profile.css'
 import QRCode from "react-qr-code";
-import { auth } from "../firebase-config"
-import { signOut } from 'firebase/auth';
+import { db, auth } from "../firebase-config"
+import { doc, setDoc, collection, getDocs } from "firebase/firestore";
 
 const Profile = () => {
-  
-  const [collection, setCollection] = useState([]);
 
-  const sampleCollection = [
-    {
-      name: "p1",
-      hometown: "Los Angeles, CA",
-      timemet: "12:43:23",
-      notes: "insert bio"
-    },
-    {
-      name: "p2",
-      hometown: "Los Angefdsfsd, CA",
-      timemet: "12:43:23",
-      notes: "insert bfdsfsdfsdio"
-    },
-  ]
+  const userColRef = collection(db, "users");
 
+  // Retrieve profile info when page loads
   useEffect(() => {
-    setCollection(sampleCollection);
-  }, [])
+    getDocs(userColRef)
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        if (doc.id == auth.currentUser.uid) {
+          document.getElementById("nameInput").value = doc.data().name;
+          document.getElementById("hometownInput").value = doc.data().hometown;
+          document.getElementById("remarksInput").value = doc.data().remarks;
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }, []);
+
+  const updateProfile = async () => {
+    // Add/update to Cloud Firestore
+    await setDoc(doc(db, "users", auth.currentUser.uid), {
+      name: document.getElementById("nameInput").value,
+      hometown: document.getElementById("hometownInput").value,
+      remarks: document.getElementById("remarksInput").value,
+    });
+  }
 
   return (
     <div id="profilePage">
@@ -39,32 +46,30 @@ const Profile = () => {
             <img src={face} alt="My Image" className="face"></img>
             <img src={thumbtack} alt="My Image" className="thumb"></img>
           </div> */}
-          <header className="old-paper">
+          <header className="old-paper"> <br></br>
+            <div className="typed-text">Name:</div>
+            <input
+              id="nameInput"
+              className="old-text"
+              placeholder="(insert name)"
+            />
             <br></br>
-            <div className="typed-text">
-              Name:
-            </div>
-            <input id="nameInput" className="old-text" placeholder="(insert name)"></input>
+            <div className="typed-text">Home-Town:</div>
+            <input
+              id="hometownInput"
+              className="old-text"
+              placeholder="(insert hometown)"
+            />
             <br></br>
-            <div className="typed-text">
-              Home-Town:
-            </div>
-            <input id="nameInput" className="old-text" placeholder="(insert hometown)"></input>
-            <br></br>
-            <div className="typed-text">
-              Remarks:
-            </div>
-            <br></br>
-            <textarea className="old-text" placeholder='(insert about-me)'></textarea>
-            <a
-              className="App-link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            </a>
+            <div className="typed-text">Remarks:</div><br></br>
+            <textarea
+              id="remarksInput"
+              className="old-text"
+              placeholder='(insert about-me)'
+              ></textarea>
           </header>
         </div>
-        <button id='savebutton'>Save Card</button> 
+        <button id='savebutton' onClick={updateProfile}>Save Card</button> 
       </div>
       <div id="qr-section">
         <p>This is your QR code. Print it out and let people scan it!</p>
@@ -77,11 +82,11 @@ const Profile = () => {
         <div id="collectionWrapper">
           <div id="collection">
           <h1>my card collection</h1>
-            {collection.map((user) => {
+            {/* {collection.map((user) => {
               return (
                 <Card name={user.name} hometown={user.hometown} notes={user.notes}/>
               )
-            })}
+            })} */}
           </div>
         </div>
       </div>
